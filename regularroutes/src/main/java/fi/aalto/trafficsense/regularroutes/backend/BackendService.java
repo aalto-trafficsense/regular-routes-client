8629905070eed80527b78b662665ae529bf199d9
Joonas Javanainen <joonas.javanainen@aalto.fi>
2014-10-11 17:12:35 +0300

    Bind FunfManager to BackendService lifecycle

--- regularroutes/src/main/java/fi/aalto/trafficsense/regularroutes/backend/BackendService.java
+++ regularroutes/src/main/java/fi/aalto/trafficsense/regularroutes/backend/BackendService.java
@@ -2,12 +2,30 @@ package fi.aalto.trafficsense.regularroutes.backend;
 
 import android.app.Service;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.IBinder;
+import edu.mit.media.funf.FunfManager;
 import fi.aalto.trafficsense.regularroutes.util.LocalBinder;
+import fi.aalto.trafficsense.regularroutes.util.LocalServiceConnection;
 import timber.log.Timber;
 
 public class BackendService extends Service {
     private final IBinder mBinder = new LocalBinder<BackendService>(this);
+
+    private final ServiceConnection mFunfServiceConnection = new LocalServiceConnection<FunfManager>() {
+
+        @Override
+        protected FunfManager getService(IBinder binder) {
+            return ((FunfManager.LocalBinder) binder).getManager();
+        }
+
+        @Override
+        protected void onService(FunfManager service) {
+            mFunfManager = service;
+        }
+    };
+
+    private FunfManager mFunfManager;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -17,6 +35,9 @@ public class BackendService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        bindService(new Intent(this, FunfManager.class), mFunfServiceConnection, BIND_AUTO_CREATE);
+
         // TODO: Start backend stuff
 
         Timber.d("Service started");
@@ -25,6 +46,8 @@ public class BackendService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+
+        unbindService(mFunfServiceConnection);
 
         Timber.d("Service stopped");
     }
