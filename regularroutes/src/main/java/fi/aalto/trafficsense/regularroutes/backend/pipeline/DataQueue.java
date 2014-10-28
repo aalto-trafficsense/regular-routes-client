930018e5b8e96f4830332ab0cf2a34bbe58c3056
Joonas Javanainen <joonas.javanainen@aalto.fi>
2014-10-28 21:04:18 +0200

    Use proper eviction for data queue

--- regularroutes/src/main/java/fi/aalto/trafficsense/regularroutes/backend/pipeline/DataQueue.java
+++ regularroutes/src/main/java/fi/aalto/trafficsense/regularroutes/backend/pipeline/DataQueue.java
@@ -1,27 +1,25 @@
 package fi.aalto.trafficsense.regularroutes.backend.pipeline;
 
+import com.google.common.collect.EvictingQueue;
 import fi.aalto.trafficsense.regularroutes.backend.parser.LocationData;
 import timber.log.Timber;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Queue;
 
 public class DataQueue implements DataCollector.Listener {
-    private final Deque<DataPoint> mDeque;
+    private static final int MAX_SIZE = 256;
+    private final Queue<DataPoint> mDeque;
 
     private long mNextSequence;
 
     public DataQueue() {
-        this.mDeque = new ArrayDeque<DataPoint>();
+        this.mDeque = EvictingQueue.create(MAX_SIZE);
     }
 
     @Override
     public void onDataReady(LocationData locationData) {
         DataPoint dataPoint = new DataPoint(System.currentTimeMillis(), mNextSequence++, locationData);
-        while (!this.mDeque.offer(dataPoint)) {
-            DataPoint discarded = this.mDeque.pop();
-            Timber.w("Queue full, discarding data point %s", discarded);
-        }
+        this.mDeque.add(dataPoint);
         Timber.d("%s", this.mDeque);
     }
 
