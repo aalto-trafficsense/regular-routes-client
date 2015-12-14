869262f2a894afb088fe35b9086dd70e898309b0
Mikko Rinne <mikko.rinne@aalto.fi>
2015-12-14 17:48:00 +0200

    One more file.

--- regularroutes/src/main/java/fi/aalto/trafficsense/regularroutes/util/RRBootReceiver.java
+++ regularroutes/src/main/java/fi/aalto/trafficsense/regularroutes/util/RRBootReceiver.java
@@ -1,7 +1,30 @@
 package fi.aalto.trafficsense.regularroutes.util;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import fi.aalto.trafficsense.regularroutes.ui.LoginActivity;
+import fi.aalto.trafficsense.regularroutes.ui.MainActivity;
+import timber.log.Timber;
+
 /**
- * Created by rinnem2 on 14/12/15.
+ * Created by mikko.rinne@aalto.fi on 14/12/15.
  */
-public class RRBootReceiver {
+public class RRBootReceiver extends BroadcastReceiver
+{
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Timber.d("RRBootReceiver called!");
+        if(intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)){
+            Timber.d("Starting main activity");
+            Intent mainIntent = new Intent(context, MainActivity.class);
+            mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(mainIntent);
+            /* Intent loginIntent = new Intent(context, LoginActivity.class);
+            loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(loginIntent); */
+        }
+    }
+
 }
